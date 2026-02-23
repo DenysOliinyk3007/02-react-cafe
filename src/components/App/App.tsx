9fd235db54b931f03e1e9860ee0beb2e4da29535
Denys Oliinyk <oliinyk@biochem.mpg.de>
2026-02-23 21:16:42 +0100

    optimize more

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import css from './App.module.css'
 import CafeInfo from '../CafeInfo/CafeInfo';
 import VoteOptions from '../VoteOptions/VoteOptions';
-import type { Votes } from '../../types/votes';
+import type { Votes, VoteType } from '../../types/votes';
 import { useState } from 'react';
 import VoteStats from '../VoteStats/VoteStats';
 import Notification from '../Notification/Notification';
@@ -19,7 +19,7 @@ export default function App(){
     })
 
 
-    const handleVote = (type: keyof Votes) => {
+    const handleVote = (type: VoteType) => {
         setVotes({
             ...votes,
             [type]: votes[type] +1,
